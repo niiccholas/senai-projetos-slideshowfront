defeaaeb2e61328642470762039000915ad1d622
NICOLAS SILVA DE ALMEIDA SANTOS <24122316@dsn127m1246734.cfp127aluno.local>
2025-07-31 11:36:04 -0300

    projeto finalizado

---
+++ script.js
@@ -0,0 +1,61 @@
+'use strict'
+
+async function fetchImagens(){
+
+    const url = `http://localhost:3000/imagens`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data
+}
+
+async function preencherImagens(){
+
+    const data = await fetchImagens()
+    const fotoHTML = document.getElementsByClassName('foto')[0]
+    const legenda = document.getElementsByClassName('legenda')[0]
+
+    let contador = 1
+
+    fotoHTML.src = data[0].foto
+    legenda.textContent = data[0].legenda
+
+    setInterval(() => {
+
+        if(contador == 4){
+            contador = 0
+        }
+
+        fotoHTML.style.opacity = 0
+
+        setTimeout(() => {
+            
+        fotoHTML.src = data[contador].foto
+        legenda.textContent = data[contador].legenda
+
+        fotoHTML.onload = () => {
+            fotoHTML.style.opacity = 1;
+        };
+
+        }, 200);
+
+        console.log(contador)
+
+        contador++
+
+
+
+        
+    }, 3700);
+
+
+    console.log(data.length)
+
+    console.log(await fetchImagens())
+
+
+}
+window.onload = async function() {
+    await preencherImagens()
+};
